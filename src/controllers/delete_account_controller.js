6c8cae3c649286e15742556d4aa639d0729d45a6
Juliana <jumiranda0501@gmail.com>
2021-08-05 13:31:52 -0300

    Delete user

---
+++ src/controllers/delete_account_controller.js
@@ -0,0 +1,52 @@
+import User from '../database/userModel';
+const debug = require('debug')('app:delete-user');
+const debugDb = require('debug')('app:database');
+
+export const delete_account = async (req, res, next) => {
+
+  const userId = req.params.userId;
+
+  try{
+
+    debug('Deleting account...');
+
+    // TODO: delete posts
+    // TODO: delete likes
+    // TODO: delete follows
+    // TODO: delete comments
+    // TODO: notifications
+    // TODO: delete watchlist
+
+    await deleteUser(userId);
+
+    debug('Account successfully deleted!');
+
+    return res.json({message: "Account deleted!"});
+  }
+  catch (error) {
+    debug('User not found.');
+    return next(error);
+  }
+
+};
+
+/* ______________ DATABASE ______________ */
+
+
+const deleteUser = async (userId) => {
+
+  const del = await User.deleteOne({_id: userId});
+
+  const count = del.deletedCount;
+
+  if (count === 0) {
+    const error = new Error(`${userId} not found`);
+    error.status = 404;
+    throw error;
+  }
+  else {
+    debugDb('User deleted');
+    return count;
+  }
+
+};
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 import { home } from './controllers/home_controller';
 import { login } from './controllers/login_controller';
 import { edit_user } from './controllers/edit_user_controller';
+import { delete_account } from './controllers/delete_account_controller';
 
 // todo: middleware to validate api key
 import { validateSignup, validateEditUser } from './helpers/inputValidator';
@@ -22,6 +23,7 @@ router.post('/api/login', validateSignup, login);
 /* ------- USER ------- */
 
 router.post('/api/user/edit-user/:userId', validateEditUser, edit_user);
+router.post('/api/user/delete-account/:userId', delete_account);
 
 
 module.exports = router;
